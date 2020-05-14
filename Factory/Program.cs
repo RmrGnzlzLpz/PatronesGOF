e98d0ec4d9ab9f5f3f1cf6ab38b49077bb449fb6
RmrGnzlzLpz <rmrgnzlzpz@gmail.com>
2020-05-14 17:39:41 -0500

    Refactorizando.

--- Factory/Program.cs
+++ Factory/Program.cs
@@ -5,14 +5,6 @@ using Factory.Interfaces;
 using System;
 using System.Collections.Generic;
 
-/*
- * Descripcion del problema:
- * Queremos que en nuestro videojuego se generen enemigos de diferentes maneras.
- * Algunos jugadores quieren que los enemigos generados sean mas dificiles, otros quieren que se generen de modo normal.
- * Para ello construimos factorías que nos permiten generar enemigos de diferentes formas, incluso podríamos generar
- * más factorías para generar solo Ninjas, o solo Samurais; y el cliente al trabajar con polimorfismo, no se ve afectado.
-*/
-
 namespace Factory
 {
     class Program
--- PatronesGOF.Test/FactoryTest.cs
+++ PatronesGOF.Test/FactoryTest.cs
@@ -5,6 +5,14 @@ using NUnit.Framework;
 
 namespace Factory.Test
 {
+    /*
+ * Descripcion del problema:
+ * Queremos que en nuestro videojuego se generen enemigos de diferentes maneras.
+ * Algunos jugadores quieren que los enemigos generados sean mas dificiles, otros quieren que se generen de modo normal.
+ * Para ello construimos factorías que nos permiten generar enemigos de diferentes formas, incluso podríamos generar
+ * más factorías para generar solo Ninjas, o solo Samurais; y el cliente al trabajar con polimorfismo, no se ve afectado.
+*/
+
     public class FactoryTests
     {
         IEnemigoFactory Factory { get; set; }
@@ -26,7 +34,7 @@ namespace Factory.Test
         [Test]
         public void CrearNinja()
         {
-            Factory = new NinjaEnemigoFactory();
+            Factory = new NinjaEnemigoFactory(); // Podemos crear cuántas factories sean necesarias.
             var enemy = Factory.CrearEnemigo();
 
             Assert.IsTrue(enemy is Ninja);
